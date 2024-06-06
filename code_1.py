28083e0ecef26b33aa54e36925b0730a4d17c6ce
Demon <you@awe.com>
2024-06-06 20:43:00 +0300

    """First push"""

--- code_1.py
+++ code_1.py
@@ -1,1 +1,28 @@
-print('Hello world_2!')
+# the first section
+for countdown in 5, 4, 3, 2, 1, 'hey!':
+    print(countdown)
+
+
+# second
+spells = [
+    'Riddikulus!',
+    'Wingardium Leviosa!',
+    'Avada Kedavra!',
+    'Expecto Patronum!',
+    'Nox!',
+    'Lumos!',
+]
+print(spells[3])
+
+# third
+quotes = {
+    'Moe': 'A wise guy, huh7',
+    'Larry': 'Ow!',
+    'Curly': 'Nyuk nyuk!',
+}
+stooge = 'Curly'
+print(stooge, 'says:', quotes[stooge])
+
+#fourth
+language = 9
+print(f"Language {language}: I am Python. What's for supper?")
