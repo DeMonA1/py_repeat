e892c61285ec9eb3ced96073689804d6fa83d808
Demon <you@awe.com>
2024-06-07 22:18:17 +0300

    '''Local and globals variables, decorators'''

--- functions.py
+++ functions.py
@@ -3,3 +3,46 @@ def edit_word(words, func):
         print(func(word))
 words = ['letted','dasd','hello']
 edit_word(words, lambda word: word.capitalize() + '!')
+
+# the decorator
+def document_it(func):
+    def new_function(*args, **kwargs):
+        print('Running function:', func.__name__)
+        print('Positional arguments:', args)
+        print('Keyword arguments:', kwargs)
+        result = func(*args, **kwargs)
+        print('Result:', result)
+        return result
+    return new_function
+
+def add_ints(a,b):
+    return a + b
+
+add_ints(3,5)
+cooler_add_ints = document_it(add_ints)     #create the decorator
+cooler_add_ints(3,5)
+
+#test
+@document_it
+def add_ints(a,b):
+    return a + b
+add_ints(3,5)
+
+def square_it(func):
+    def new_function(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result * result
+    return new_function
+
+#test
+@document_it
+@square_it
+def add_ints(a, b):
+    return a + b
+add_ints(3,5)
+
+@square_it
+@document_it
+def add_ints(a, b):
+    return a + b
+add_ints(3,5)
