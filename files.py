39651669eca4b045208b1de5f2a5b14de47eba89
Demon <you@awe.com>
2024-06-13 22:41:55 +0300

    '''

--- files.py
+++ files.py
@@ -1,4 +1,5 @@
-import os,shutil
+import os,shutil, glob
+from pathlib import Path
 
 fout = open('oops.txt','wt')
 print('Oops, I created a file.', file=fout)
@@ -160,3 +161,65 @@ os.mkdir('poems')
 os.path.exists('poems')
 os.rmdir('poems')
 os.path.exists('poems')
+
+os.mkdir('poems')
+os.listdir('poems')
+os.mkdir('poems/mcintyre')
+os.listdir('poems')
+fout = open('poems/mcintyre/the_good_man', 'wt')
+fout.write("""'Cheerful and happy was his mood,
+He to the poor was kind and good,
+And he oft' times did find them food,
+Also supplies of coal and wood,
+He never spake a word was rude,
+And cheer'd those did o'er sorrows brood,
+He passed away not understood,
+Because no poet in his lays
+Had penned a sonnet in his praise,
+'Tis sad, but such is world's ways.
+""")
+fout.close()
+os.listdir('poems/mcintyre')
+
+
+os.chdir('poems')
+os.listdir('.')
+glob.glob('m*')
+glob.glob('??')
+glob.glob('m??????e')
+glob.glob('[klm]*e')
+
+
+os.path.abspath('oops.txt')
+os.symlink('oops.txt', 'jeepers.txt')
+os.path.islink('jeepers.txt')
+os.path.realpath('jeepers.txt')
+win_file = os.path.join('py_re', 'poems')
+win_file = os.path.join(win_file, 'today')
+win_file
+
+
+file_path = Path('py_repeat') / 'poems'/ 'lll.txt'
+file_path
+print(file_path)
+file_path.name
+file_path.suffix
+file_path.stem
+
+glob.glob('*.txt')
+
+os.chdir('..')
+glob.glob('*.txt')
+
+test1 = "This is a test of the emergency text system"
+os.chdir('./py_repeat')
+os.getcwd()
+with open('testinh.txt', 'wt') as fe:
+    fe.write(test1)
+    print('close')
+with open('testinh.txt', 'r') as fe:
+    test2 = fe.read()
+test1 == test2
+import datetime, time
+a = time.localtime()
+time.strftime('%H:%M; %d:%m:%Y',a)
